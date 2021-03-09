da06c87b98b6449a6930a10539ab78d26359f5c2
abbo-1 <abbott.kevinj@gmail.com>
2021-03-09 17:45:07 -0600

    added delete function

--- src/App.js
+++ src/App.js
@@ -9,36 +9,59 @@ function App() {
   const [tasks, setTasks] = useState([
       {
           id: 1,
-          text: 'Walk the Dog',
+          text: 'File Taxes',
           day: 'Monday',
           reminder: true,
       },
       {
-          id: 1,
-          text: 'Wash the dog',
+          id: 2,
+          text: 'Mow the Lawn',
           day: 'Tuesday',
           reminder: true,
       },
       {
-          id: 1,
-          text: 'Pet the dog',
+          id: 3,
+          text: 'Business Meeting',
           day: 'Thursday',
           reminder: false,
       },
       {
-          id: 1,
-          text: 'Feed the dog',
+          id: 4,
+          text: 'Grocery Shopping',
           day: 'Friday',
           reminder: true,
       },
+      {
+          id: 5,
+          text: 'Give Dog a Bath',
+          day: 'Saturday',
+          reminder: true,
+      }
       ]
   )
 
+  // DELETE
+  const deleteTask = (id) => {
+    console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // REMINDER
+
+  const toggleReminder = (id) => {
+    console.log(id)
+  }
 
   return (
     <div className="container">
       <Header title='Tracker'/>
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+      <Tasks 
+      tasks={tasks} 
+      onDelete={deleteTask}
+      onToggle={toggleReminder} />
+      ) : ( 'No Tasks To Show'
+      )}
 
     {/* <h1> Does {name} own space </h1> */}
     {/* <h1> {x ? 'Yes' : 'No'} </h1> */}
